d6b77a4be01bc04e91a6dd54b67a0ad2ed4e7273
Toshio Minei <sminei10@gmail.com>
2018-09-18 03:36:18 -0500

    correct typo of db_name env variable

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@ $capsule = new Capsule;
 $capsule->addConnection([
   'driver'    => getenv('DB_DRIVER'),
   'host'      => getenv('DB_HOST'),
-  'database'  => getenv('DB_DATABASE'),
+  'database'  => getenv('DB_NAME'),
   'username'  => getenv('DB_USER'),
   'password'  => getenv('DB_PASS'),
   'charset'   => 'utf8',
